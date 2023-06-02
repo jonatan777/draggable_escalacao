7b75d6d4acd554e9863ec9e783a93a141d3be550
jonatan777 <jonataneduardo777@gmail.com>
2023-06-02 18:57:23 -0300

    first commit

---
+++ form.js
@@ -0,0 +1,119 @@
+
+
+
+
+
+function Time1Function() {
+    var x = document.getElementById("form1");
+    
+    
+     document.getElementById("1").innerHTML = '<img src=./images/'+x.elements[0].value+' alt=" "></img>'
+     document.getElementById("2").innerHTML = '<img src=./images/'+x.elements[1].value+' alt=" "></img>'
+     document.getElementById("3").innerHTML = '<img src=./images/'+x.elements[2].value+' alt=" "></img>'
+     document.getElementById("4").innerHTML = '<img src=./images/'+x.elements[3].value+' alt=" "></img>'
+     document.getElementById("5").innerHTML = '<img src=./images/'+x.elements[4].value+' alt=" "></img>'
+     document.getElementById("6").innerHTML = '<img src=./images/'+x.elements[5].value+' alt=" "></img>'
+     document.getElementById("7").innerHTML = '<img src=./images/'+x.elements[6].value+' alt=" "></img>'
+     document.getElementById("8").innerHTML = '<img src=./images/'+x.elements[7].value+' alt=" "></img>'
+     document.getElementById("9").innerHTML = '<img src=./images/'+x.elements[8].value+' alt=" "></img>'
+     document.getElementById("10").innerHTML = '<img src=./images/'+x.elements[9].value+' alt=" "></img>'
+     document.getElementById("11").innerHTML = '<img src=./images/'+x.elements[10].value+' alt=" "></img>'
+     document.getElementById("12").innerHTML = '<img src=./images/'+x.elements[11].value+' alt=" "></img>'
+     document.getElementById("13").innerHTML = '<img src=./images/'+x.elements[12].value+' alt=" "></img>'
+     document.getElementById("14").innerHTML = '<img src=./images/'+x.elements[13].value+' alt=" "></img>'
+     document.getElementById("15").innerHTML = '<img src=./images/'+x.elements[14].value+' alt=" "></img>'
+     document.getElementById("16").innerHTML = '<img src=./images/'+x.elements[15].value+' alt=" "></img>'
+     document.getElementById("17").innerHTML = '<img src=./images/'+x.elements[16].value+' alt=" "></img>'
+     document.getElementById("18").innerHTML = '<img src=./images/'+x.elements[17].value+' alt=" "></img>'
+     document.getElementById("19").innerHTML = '<img src=./images/'+x.elements[18].value+' alt=" "></img>'
+     document.getElementById("20").innerHTML = '<img src=./images/'+x.elements[19].value+' alt=" "></img>'
+     
+
+  }
+
+
+  function Time2Function() {
+    var x = document.getElementById("form2");
+    
+     document.getElementById("21").innerHTML = '<img src=./images/'+x.elements[0].value+' alt=" "></img>'
+     document.getElementById("22").innerHTML = '<img src=./images/'+x.elements[1].value+' alt=" "></img>'
+     document.getElementById("23").innerHTML = '<img src=./images/'+x.elements[2].value+' alt=" "></img>'
+     document.getElementById("24").innerHTML = '<img src=./images/'+x.elements[3].value+' alt=" "></img>'
+     document.getElementById("25").innerHTML = '<img src=./images/'+x.elements[4].value+' alt=" "></img>'
+     document.getElementById("26").innerHTML = '<img src=./images/'+x.elements[5].value+' alt=" "></img>'
+     document.getElementById("27").innerHTML = '<img src=./images/'+x.elements[6].value+' alt=" "></img>'
+     document.getElementById("28").innerHTML = '<img src=./images/'+x.elements[7].value+' alt=" "></img>'
+     document.getElementById("29").innerHTML = '<img src=./images/'+x.elements[8].value+' alt=" "></img>'
+     document.getElementById("30").innerHTML = '<img src=./images/'+x.elements[9].value+' alt=" "></img>'
+     document.getElementById("31").innerHTML = '<img src=./images/'+x.elements[10].value+' alt=" "></img>'
+     document.getElementById("32").innerHTML = '<img src=./images/'+x.elements[11].value+' alt=" "></img>'
+     document.getElementById("33").innerHTML = '<img src=./images/'+x.elements[12].value+' alt=" "></img>'
+     document.getElementById("34").innerHTML = '<img src=./images/'+x.elements[13].value+' alt=" "></img>'
+     document.getElementById("35").innerHTML = '<img src=./images/'+x.elements[14].value+' alt=" "></img>'
+     document.getElementById("36").innerHTML = '<img src=./images/'+x.elements[15].value+' alt=" "></img>'
+     document.getElementById("37").innerHTML = '<img src=./images/'+x.elements[16].value+' alt=" "></img>'
+     document.getElementById("38").innerHTML = '<img src=./images/'+x.elements[17].value+' alt=" "></img>'
+     document.getElementById("39").innerHTML = '<img src=./images/'+x.elements[18].value+' alt=" "></img>'
+     document.getElementById("40").innerHTML = '<img src=./images/'+x.elements[19].value+' alt=" "></img>'
+
+     /**
+     document.getElementById("41").innerHTML = '<img src='+x.elements[13].value+' alt=""></img>'
+     document.getElementById("42").innerHTML = '<img src='+x.elements[14].value+' alt=""></img>'
+     document.getElementById("43").innerHTML = '<img src='+x.elements[15].value+' alt=""></img>'
+     document.getElementById("44").innerHTML = '<img src='+x.elements[16].value+' alt=""></img>'
+     document.getElementById("45").innerHTML = '<img src='+x.elements[17].value+' alt=""></img>'
+     document.getElementById("46").innerHTML = '<img src='+x.elements[18].value+' alt=""></img>'
+     document.getElementById("47").innerHTML = '<img src='+x.elements[19].value+' alt=""></img>'
+     document.getElementById("48").innerHTML = '<img src='+x.elements[20].value+' alt=""></img>'
+     document.getElementById("49").innerHTML = '<img src='+x.elements[21].value+' alt=""></img>'
+     document.getElementById("50").innerHTML = '<img src='+x.elements[22].value+' alt=""></img>'
+     document.getElementById("51").innerHTML = '<img src='+x.elements[23].value+' alt=""></img>'
+     document.getElementById("52").innerHTML = '<img src='+x.elements[24].value+' alt=""></img>'
+     document.getElementById("53").innerHTML = '<img src='+x.elements[25].value+' alt=""></img>'
+     document.getElementById("54").innerHTML = '<img src='+x.elements[26].value+' alt=""></img>'
+     document.getElementById("55").innerHTML = '<img src='+x.elements[27].value+' alt=""></img>'
+     document.getElementById("56").innerHTML = '<img src='+x.elements[28].value+' alt=""></img>'
+    */
+  }
+
+
+  
+
+
+  function dataPelada() {
+    var x = document.getElementById("data_pelada");
+    var y = document.getElementById("rodada_pelada");
+    var z = document.getElementById("placar_pelada");  
+    var n =  document.getElementById("form3");  
+
+
+    document.getElementById("data").innerHTML = x.value
+    document.getElementById("data_pelada").style.display = "none"
+
+    document.getElementById("rodada").innerHTML = y.value
+    document.getElementById("rodada_pelada").style.display = "none"
+
+    document.getElementById("placar").innerHTML = z.value
+    document.getElementById("placar_pelada").style.display = "none"
+
+
+    document.getElementById("formuarios").style.display = "none"
+    document.getElementById("botao").style.display = "none"
+    document.getElementById("numbers").style.display = "none"
+    
+    
+   document.getElementById("numbers1").innerHTML = n.elements[0].value
+   document.getElementById("numbers2").innerHTML = n.elements[1].value
+   document.getElementById("numbers3").innerHTML = n.elements[2].value
+   document.getElementById("numbers4").innerHTML = n.elements[3].value
+   document.getElementById("numbers5").innerHTML = n.elements[4].value
+   document.getElementById("numbers6").innerHTML = n.elements[5].value
+   document.getElementById("numbers7").innerHTML = n.elements[6].value
+   document.getElementById("numbers8").innerHTML = n.elements[7].value
+   document.getElementById("numbers9").innerHTML = n.elements[8].value
+   document.getElementById("numbers10").innerHTML = n.elements[9].value
+
+
+   document.getElementById("form3").style.display = "none"
+
+  }
---
+++ script.js
@@ -0,0 +1,11 @@
+
+ gsap.registerPlugin(ScrollTrigger, Draggable);
+
+	//	Draggable.create("img", {type:"x,y", edgeResistance:0, bounds:"img", inertia:true});
+    Draggable.create("img");
+	Draggable.create("h3");
+	Draggable.create("h2");
+	Draggable.create("h1");
+
+
+ 
